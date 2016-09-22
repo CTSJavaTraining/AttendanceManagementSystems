4e40ca2fe96081bced38c88c3503b55eef530212
523696 <523696@PC334050.cts.com>
2016-09-22 19:47:31 +0530

    Removed try,catch block in methods.

--- src/main/java/com/attendance/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/attendance/serviceImpl/EmployeeServiceImpl.java
@@ -4,72 +4,64 @@
 package com.attendance.serviceImpl;
 
 import java.util.UUID;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.attendance.DAOServiceImpl.EmployeeDAOImpl;
 import com.attendance.entity.Employee;
 import com.attendance.entity.EmployeeId;
+
 import com.attendance.service.EmployeeService;
 
 /**
- * @author 542320 
- * EmployeeServiceImpl holds the implementation of methods used in EmployeeDAO interface.
- *         
+ * @author 542320 EmployeeServiceImpl holds the implementation of methods used
+ *         in EmployeeDAO interface.
+ * 
  */
 public class EmployeeServiceImpl implements EmployeeService {
 
 	static final Logger logger = Logger.getLogger(EmployeeServiceImpl.class);
-	
+
 	ApplicationContext context = null;
 
 	/**
 	 * 
 	 * Service method to invoke the insertEmployee of DAO class.
+	 * 
+	 * @throws Exception
 	 */
 
 	@Override
-	public void insertEmployee(Employee employee) {
+	public void insertEmployee(Employee employee) throws Exception {
 
-		try {
-			context = new ClassPathXmlApplicationContext("Beans.xml");
+		context = new ClassPathXmlApplicationContext("Beans.xml");
 
-			EmployeeDAOImpl emp = (EmployeeDAOImpl) context.getBean("employee");
+		EmployeeDAOImpl emp = (EmployeeDAOImpl) context.getBean("employee");
 
-			String accessCard = generateAccessCard();
+		String accessCard = generateAccessCard();
 
-			employee.getId().setAccessCardno(accessCard);
+		employee.getId().setAccessCardno(accessCard);
 
-			emp.insertEmployee(employee);
-
-		} catch (Exception e) {
-
-			logger.error("Encountered exception:" + e);
-
-		}
+		emp.insertEmployee(employee);
 
 	}
 
 	/**
 	 * 
 	 * Service method to invoke the deleteEmployee of DAO class.
+	 * 
+	 * @throws Exception
 	 */
 
 	@Override
-	public void deleteEmployee(EmployeeId empId) {
+	public void deleteEmployee(EmployeeId empId) throws Exception {
 
-		try {
-			 context = new ClassPathXmlApplicationContext("Beans.xml");
+		context = new ClassPathXmlApplicationContext("Beans.xml");
 
-			EmployeeDAOImpl emp = (EmployeeDAOImpl) context.getBean("employee");
+		EmployeeDAOImpl emp = (EmployeeDAOImpl) context.getBean("employee");
 
-			emp.deleteEmployee(empId);
-
-		} catch (Exception e) {
-
-			logger.error("Encountered exception:" + e);
-
-		}
+		emp.deleteEmployee(empId);
 
 	}
 
